31295c89dc7a19a0972dfd72eaddc3d691ea3ebb
LIFLIYLI <806820876@qq.com>
2018-10-17 16:43:09 +0800

    完成 webpack 的第一个例子

---
+++ build/precache-manifest.b6e7cfd945458605d37abe1c72a4e208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee90db83d43b1760c64d",
+    "url": "/todo-list/build/static/js/runtime~main.ee90db83.js"
+  },
+  {
+    "revision": "0d92c1c22af4b237e226",
+    "url": "/todo-list/build/static/js/main.0d92c1c2.chunk.js"
+  },
+  {
+    "revision": "0840999a8fcce1ba1e9d",
+    "url": "/todo-list/build/static/js/1.0840999a.chunk.js"
+  },
+  {
+    "revision": "0d92c1c22af4b237e226",
+    "url": "/todo-list/build/static/css/main.434dae36.chunk.css"
+  },
+  {
+    "revision": "0840999a8fcce1ba1e9d",
+    "url": "/todo-list/build/static/css/1.803af3cd.chunk.css"
+  },
+  {
+    "revision": "02cc31c65f5b4ed237e7edf091fa0a1f",
+    "url": "/todo-list/build/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './reset.css'
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
+import 'normalize.css'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      newTodo: '',
+      todoList: [
+        {id:1, title:'第一个待办'},
+        {id:2, title:'第二个待办'},
+      ]
+    }
+  }
   render() {
+    let todos = this.state.todoList
+    .filter((item)=> !item.deleted)
+    .map((item,index)=>{
+      return ( // 为什么这里要加个括号？这是动手题3 🐸
+        <li key={index}>
+         <TodoItem todo={item} onToggle={this.toggle.bind(this)} 
+            onDelete={this.delete.bind(this)}/>
+        </li>
+      )
+    })
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         <h1>我的待办</h1>
+        <div className="inputWrapper">
+        <TodoInput content={this.state.newTodo} 
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
+        </div>
+        <ol className="todoList">
+          {todos}
+        </ol>
       </div>
-    );
+    )
+  }
+  toggle(e, todo){
+    todo.status = todo.status === 'completed' ? '' : 'completed'
+    this.setState(this.state) 
+  }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
+  delete(event, todo){
+    todo.deleted = true
+    this.setState(this.state) 
   }
 }
 
 export default App;
+
+let id = 0
+ function idMaker(){
+  id += 1
+  return id
+}
